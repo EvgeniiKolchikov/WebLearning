5280b9ade9cea8200163e4361075699068949ec8
= <evgeniy_kolchikov@mail.ru>
2023-02-08 21:46:35 +0300

    Configure Lesson

--- WebLessons/ConfigureLesson/Program.cs
+++ WebLessons/ConfigureLesson/Program.cs
@@ -1,25 +1,57 @@
+using System.Text;
+using Microsoft.Extensions.Options;
+
 string[] commandLineArgs = { "name=Sam", "age=33" };
 
-//var builder = WebApplication.CreateBuilder(commandLineArgs);
-var builder = WebApplication.CreateBuilder();
-builder.Configuration.AddCommandLine(commandLineArgs);
 
-// builder.Configuration.AddInMemoryCollection(new Dictionary<string, string>
-// {
-//     { "name", "Max" },
-//     { "age", "17" }
-// });
+var builder = WebApplication.CreateBuilder();
+
+
+
+builder.Configuration.AddJsonFile("person.json");
+builder.Services.Configure<Person>(builder.Configuration);
 
 var app = builder.Build();
+//app.Map("/", (IConfiguration appConfig) => GetSectionContent(appConfig.GetSection("projectConfig")));
 
-app.Map("/", (IConfiguration appConfig) => $"{appConfig["name"]} + {appConfig["age"]}");
-
-// app.Run(async (context) =>
-// {
-//     var name = app.Configuration["name"];
-//     var age = app.Configuration["age"];
-//     await context.Response.WriteAsync($"{name} - {age}");
-// });
+app.Map("/", (IOptions<Person> options) =>
+{
+    var person = options.Value;
+    return person;
+});
 
 
 app.Run();
+
+string GetSectionContent(IConfiguration configSection)
+{
+    var contentBuilder = new StringBuilder();
+
+    foreach (var section in configSection.GetChildren())
+    {
+        contentBuilder.Append($"\"{section.Key}\":");
+        if (section.Value == null)
+        {
+            string subSectionContent = GetSectionContent(section);
+            contentBuilder.Append($"{{\n{subSectionContent}}},\n");
+        }
+        else
+        {
+            contentBuilder.Append($"\"{section.Value}\",\n");
+        }
+    }
+    return contentBuilder.ToString();
+}
+
+public class Person
+{
+    public string Name { get; set; } = "";
+    public int Age { get; set; }
+    public List<string> Languages { get; set; } = new();
+    public Company? Company { get; set; }
+}
+public class Company
+{
+    public string Title { get; set; } = "";
+    public string Country { get; set; } = "";
+}
